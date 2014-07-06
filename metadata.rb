61178b542d11ffd6d9dec58f7928867774c8b9db
Evan Sosenko <razorx@evansosenko.com>
2014-07-06 00:09:00 -0700

    Add php to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -10,5 +10,6 @@ recipe 'phpmyadmin::default', 'Configures phpMyAdmin.'
 depends 'core', '~> 0.0.0'
 depends 'git', '~> 4.0.2'
 depends 'mysql', '~> 5.3.6'
+depends 'php', '~> 1.4.7'
 
 provides 'service[phpmyadmin]'
